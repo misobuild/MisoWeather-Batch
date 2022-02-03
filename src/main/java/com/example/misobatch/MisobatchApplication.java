44e4266d1d53645508b308e2dd746d883113e45f
tmddusgood <tmddusgood@gmail.com>
2022-02-03 17:11:01 +0900

    first commit

---
+++ src/main/java/com/example/misobatch/MisobatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.misobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisobatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisobatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/misobatch/MisobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.misobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisobatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
