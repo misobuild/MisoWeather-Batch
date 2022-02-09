4b0d1314edde19be6112d75db2fa21d9903d0b91
tmddusgood <tmddusgood@gmail.com>
2022-02-09 15:45:13 +0900

    refactor: 추가 MainFlowService: registerMember()

--- misoweather-service/src/main/java/com/misoweather/misoweatherservice/service/MainFlowService.java
+++ misoweather-service/src/main/java/com/misoweather/misoweatherservice/service/MainFlowService.java
@@ -3,15 +3,19 @@ package com.misoweather.misoweatherservice.service;
 import com.misoweather.misoweatherservice.constants.HttpStatusEnum;
 import com.misoweather.misoweatherservice.domain.member.Member;
 import com.misoweather.misoweatherservice.domain.member_region_mapping.MemberRegionMapping;
+import com.misoweather.misoweatherservice.domain.region.Region;
 import com.misoweather.misoweatherservice.dto.request.comment.CommentRegisterRequestDto;
 import com.misoweather.misoweatherservice.dto.request.member.DeleteMemberRequestDto;
 import com.misoweather.misoweatherservice.dto.request.member.SignUpRequestDto;
 import com.misoweather.misoweatherservice.dto.response.comment.CommentRegisterResponseDto;
 import com.misoweather.misoweatherservice.dto.response.member.MemberInfoResponseDto;
 import com.misoweather.misoweatherservice.exception.ApiCustomException;
+import com.misoweather.misoweatherservice.utils.factory.ValidatorFactory;
+import com.misoweather.misoweatherservice.utils.validator.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -24,8 +28,14 @@ public class MainFlowService {
     private final RegionService regionService;
     private final SurveyService surveyService;
 
+    @Transactional
     public Member registerMember(SignUpRequestDto signUpRequestDto, String socialToken){
-
+        memberService.checkToken(signUpRequestDto.getSocialId(), signUpRequestDto.getSocialType(), socialToken);
+        memberService.checkExistence(signUpRequestDto.getSocialId(), signUpRequestDto.getSocialType(), signUpRequestDto.getNickname());
+        Member registeredMember = memberService.buildMemberAndSave(signUpRequestDto);
+        Region defaultRegion = regionService.getRegion(signUpRequestDto.getDefaultRegionId());
+        mappingService.buildMemberRegionMappingAndSave(registeredMember, defaultRegion);
+        return registeredMember;
     }
 
     // 원래 memberService의 delete였음
