187263d167f39d08c5c81502bff8798b58d38f28
tmddusgood <tmddusgood@gmail.com>
2022-02-10 15:15:13 +0900

    refactor: 수정 MemberService

--- misoweather-service/src/main/java/com/misoweather/misoweatherservice/service/MemberService.java
+++ misoweather-service/src/main/java/com/misoweather/misoweatherservice/service/MemberService.java
@@ -1,14 +1,12 @@
 package com.misoweather.misoweatherservice.service;
 
 import com.misoweather.misoweatherservice.auth.JwtTokenProvider;
-import com.misoweather.misoweatherservice.auth.KakaoOAuth;
 import com.misoweather.misoweatherservice.constants.BigScaleEnum;
 import com.misoweather.misoweatherservice.constants.HttpStatusEnum;
 import com.misoweather.misoweatherservice.domain.member.Member;
 import com.misoweather.misoweatherservice.domain.member.MemberRepository;
 import com.misoweather.misoweatherservice.domain.member_region_mapping.MemberRegionMapping;
 import com.misoweather.misoweatherservice.domain.nickname.*;
-import com.misoweather.misoweatherservice.dto.request.member.LoginRequestDto;
 import com.misoweather.misoweatherservice.dto.request.member.SignUpRequestDto;
 import com.misoweather.misoweatherservice.dto.response.member.MemberInfoResponseDto;
 import com.misoweather.misoweatherservice.dto.response.member.NicknameResponseDto;
@@ -25,7 +23,6 @@ import java.util.concurrent.ThreadLocalRandom;
 @Service
 public class MemberService {
 
-    // TODO 필드 생성자 추천하지 않음
     private final MemberRepository memberRepository;
     private final AdjectiveRepository adjectiveRepository;
     private final AdverbRepository adverbRepository;
@@ -40,13 +37,10 @@ public class MemberService {
     }
 
     public NicknameResponseDto buildNickname() {
-        // TODO RANDOM sql 사용하면 좋을 것 같다.
         Adjective adjective = adjectiveRepository.findById(getRandomId(adjectiveRepository.count()))
                 .orElseThrow(() -> new ApiCustomException(HttpStatusEnum.NOT_FOUND));
-
         Adverb adverb = adverbRepository.findById(getRandomId(adverbRepository.count()))
                 .orElseThrow(() -> new ApiCustomException(HttpStatusEnum.NOT_FOUND));
-
         Emoji emoji = emojiRepository.findById(getRandomId(emojiRepository.count()))
                 .orElseThrow(() -> new ApiCustomException(HttpStatusEnum.NOT_FOUND));
 
@@ -63,25 +57,7 @@ public class MemberService {
                 .emoji(signUpRequestDto.getEmoji())
                 .nickname(signUpRequestDto.getNickname())
                 .build();
-
         return memberRepository.save(member);
-    }
-
-    public String reissue(LoginRequestDto loginRequestDto, String socialToken) {
-        // checkMember
-        Validator validator = ValidatorFactory
-                .of(loginRequestDto.getSocialId(), loginRequestDto.getSocialType(), socialToken);
-        Boolean temp = validator.valid();
-        if (temp.equals(Boolean.FALSE)) {
-            throw new ApiCustomException(HttpStatusEnum.BAD_REQUEST);
-        }
-
-        Member member = memberRepository
-                .findBySocialIdAndSocialType(loginRequestDto.getSocialId(), loginRequestDto.getSocialType())
-                .orElseThrow(() -> new ApiCustomException(HttpStatusEnum.NOT_FOUND));
-
-        return jwtTokenProvider
-                .createToken(Long.toString(member.getMemberId()), member.getSocialId(), member.getSocialType());
     }
 
     public MemberInfoResponseDto buildMemberInfoResponse(Member member, MemberRegionMapping memberRegionMapping) {
@@ -89,8 +65,7 @@ public class MemberService {
                 .emoji(member.getEmoji())
                 .nickname(member.getNickname())
                 .regionId(memberRegionMapping.getRegion().getId())
-                .regionName(BigScaleEnum
-                        .getEnum(memberRegionMapping.getRegion().getBigScale()).toString())
+                .regionName(BigScaleEnum.getEnum(memberRegionMapping.getRegion().getBigScale()).toString())
                 .build();
     }
 
@@ -98,7 +73,6 @@ public class MemberService {
         int randomNumber = ThreadLocalRandom
                 .current()
                 .nextInt(1, Long.valueOf(number).intValue() + 1);
-
         return Long.valueOf(randomNumber);
     }
 
@@ -109,13 +83,9 @@ public class MemberService {
 
     public void checkExistence(String socialId, String socialType, String nickname) {
         memberRepository.findBySocialIdAndSocialType(socialId, socialType)
-                .ifPresent(m -> {
-                    throw new ApiCustomException(HttpStatusEnum.CONFLICT);
-                });
+                .ifPresent(m -> { throw new ApiCustomException(HttpStatusEnum.CONFLICT); });
         memberRepository.findByNickname(nickname)
-                .ifPresent(m -> {
-                    throw new ApiCustomException(HttpStatusEnum.CONFLICT);
-                });
+                .ifPresent(m -> { throw new ApiCustomException(HttpStatusEnum.CONFLICT); });
     }
 
     public String createToken(Member member){
