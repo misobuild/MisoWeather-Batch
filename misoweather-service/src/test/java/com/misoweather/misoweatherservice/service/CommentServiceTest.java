9fcc4372439cc75b851abc41ff82db277f65f4d8
tmddusgood <tmddusgood@gmail.com>
2022-02-25 20:40:20 +0900

    test: 삭제 CommentServiceTest: getCommentsWhenListNull()

--- misoweather-service/src/test/java/com/misoweather/misoweatherservice/service/CommentServiceTest.java
+++ misoweather-service/src/test/java/com/misoweather/misoweatherservice/service/CommentServiceTest.java
@@ -3,10 +3,7 @@ package com.misoweather.misoweatherservice.service;
 import com.misoweather.misoweatherservice.constants.BigScaleEnum;
 import com.misoweather.misoweatherservice.domain.comment.Comment;
 import com.misoweather.misoweatherservice.domain.comment.CommentRepository;
-import com.misoweather.misoweatherservice.domain.member.Member;
-import com.misoweather.misoweatherservice.dto.response.comment.CommentRegisterResponseDto;
 import com.misoweather.misoweatherservice.utils.reader.ContentReader;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,13 +16,14 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.nullValue;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 //TODO assertThat 으로 교체
 @ExtendWith(MockitoExtension.class)
@@ -43,110 +41,44 @@ public class CommentServiceTest {
     }
 
     @Test
-    @DisplayName("saveComment() 테스트")
-    void saveComment(){
-        Member givenMember = Member.builder()
-                .socialId("12345")
-                .emoji("a")
-                .nickname("행복한 가짜광대")
-                .socialType("kakao")
-                .build();
-
-        Comment givenComment = Comment.builder()
-                .content(contentReader.checker("안녕하세요"))
-                .bigScale(BigScaleEnum.getEnum("서울특별시").toString())
-                .member(givenMember)
-                .nickname(givenMember.getNickname())
-                .deleted(Boolean.FALSE)
-                .emoji(givenMember.getEmoji())
-                .build();
-
-        given(commentRepository.save(any(Comment.class))).willReturn(givenComment);
-
-        Comment savedComment = commentService.saveComment("안녕하세요", givenMember, "서울특별시");
-
-        assertThat(savedComment, is(givenComment));
-    }
-
-    @Test
-    @DisplayName("getAllCommentList() 테스트")
-    void getAllComment(){
-        Member givenMember = Member.builder()
-                .socialId("12345")
-                .emoji("a")
-                .nickname("행복한 가짜광대")
-                .socialType("kakao")
-                .build();
-
-        Comment givenComment = Comment.builder()
-                .content(contentReader.checker("안녕하세요"))
-                .bigScale(BigScaleEnum.getEnum("서울특별시").toString())
-                .member(givenMember)
-                .nickname(givenMember.getNickname())
-                .deleted(Boolean.FALSE)
-                .emoji(givenMember.getEmoji())
-                .build();
-
-        given(commentRepository.findAll()).willReturn(List.of(givenComment));
-
-        CommentRegisterResponseDto resultDto = commentService.getAllCommentList();
-
-        assertThat(resultDto.getCommentList().get(0), is(givenComment));
-    }
-
-    @Test
     @DisplayName("getComments() 테스트")
     void getComments() {
-        Member givenMember = Member.builder()
-                .socialId("67890")
-                .emoji("b")
-                .nickname("우울한 진짜광대")
-                .socialType("apple")
-                .build();
-
         Comment givenComment = Comment.builder()
                 .content(contentReader.checker("안녕하세요"))
                 .bigScale(BigScaleEnum.getEnum("서울특별시").toString())
-                .member(givenMember)
-                .nickname(givenMember.getNickname())
                 .deleted(Boolean.FALSE)
-                .emoji(givenMember.getEmoji())
                 .build();
 
-        given(commentRepository.findAllByOrderByIdDesc(any(Pageable.class))).willReturn(List.of());
         given(commentRepository.findByIdLessThanOrderByIdDesc(anyLong(), any(Pageable.class))).willReturn(List.of(givenComment));
 
         List<Comment> commentListExist = commentService.getComments(1L, PageRequest.of(0,1));
+
+        assertThat(commentListExist.get(0), is(givenComment));
+    }
+
+    @Test
+    @DisplayName("getComments() when commentListNull 테스트")
+    void getCommentsWhenListNull() {
+        given(commentRepository.findAllByOrderByIdDesc(any(Pageable.class))).willReturn(List.of());
+
         List<Comment> commentListNull = commentService.getComments(null, PageRequest.of(0,1));
 
         assertThat(commentListNull, is(List.of()));
-        assertThat(commentListExist.get(0), is(givenComment));
-
-        verify(commentRepository, times(1)).findAllByOrderByIdDesc(any(Pageable.class));
-        verify(commentRepository, times(1)).findByIdLessThanOrderByIdDesc(anyLong(), any(Pageable.class));
     }
 
     @Test
     @DisplayName("hasNext() 테스트")
     void hasNextTest(){
-        Long givenFirstId = 0L;
-        Long givenSecondId = 1L;
-
         given(commentRepository.existsByIdLessThan(0L)).willReturn(Boolean.FALSE);
         given(commentRepository.existsByIdLessThan(1L)).willReturn(Boolean.TRUE);
 
-        Boolean firstResult = commentService.hasNext(givenFirstId);
-        Boolean secondResult = commentService.hasNext(givenSecondId);
+        Boolean firstResult = commentService.hasNext(0L);
+        Boolean secondResult = commentService.hasNext(1L);
         Boolean thirdResult = commentService.hasNext(null);
-
-        Assertions.assertEquals(firstResult, Boolean.FALSE);
-        Assertions.assertEquals(secondResult, Boolean.TRUE);
-        Assertions.assertEquals(thirdResult, Boolean.FALSE);
 
         assertThat(firstResult, is(Boolean.FALSE));
         assertThat(secondResult, is(Boolean.TRUE));
         assertThat(thirdResult, is(Boolean.FALSE));
-
     }
 
     @Test
