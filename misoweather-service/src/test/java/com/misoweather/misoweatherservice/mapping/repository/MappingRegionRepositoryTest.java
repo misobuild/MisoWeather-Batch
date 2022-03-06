912a4b933c70f8f72fea34c1579e275aff0d9244
tmddusgood <tmddusgood@gmail.com>
2022-03-06 13:23:14 +0900

    test: 변경 MappingRegionRepositoryTest

--- misoweather-service/src/test/java/com/misoweather/misoweatherservice/mapping/repository/MappingRegionRepositoryTest.java
+++ misoweather-service/src/test/java/com/misoweather/misoweatherservice/mapping/repository/MappingRegionRepositoryTest.java
@@ -5,8 +5,6 @@ import com.misoweather.misoweatherservice.domain.member_region_mapping.MemberReg
 import com.misoweather.misoweatherservice.domain.member_region_mapping.MemberRegionMappingRepository;
 import com.misoweather.misoweatherservice.domain.region.Region;
 import com.misoweather.misoweatherservice.global.constants.RegionEnum;
-import com.misoweather.misoweatherservice.mapping.service.MappingRegionService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,16 +26,9 @@ public class MappingRegionRepositoryTest {
     @Autowired
     private EntityManager entityManager;
 
-    private MappingRegionService mappingRegionService;
-
-    @BeforeEach
-    void setUp(){
-        this.mappingRegionService = new MappingRegionService(memberRegionRepository);
-    }
-
     @Test
-    @DisplayName("성공: <Member> 객체로 <MemberRegionMapping> 리스트 찾아 반환한다.")
-    void getMemberRegionMappingList(){
+    @DisplayName("성공: <MemberRegionMapping>을 <Member>로 찾아서 반환한다.")
+    void findMemberRegionMappingByMember(){
         // given
         Member givenMember = Member.builder()
                 .socialType("kakao")
@@ -57,88 +48,11 @@ public class MappingRegionRepositoryTest {
         entityManager.persist(givenMemberRegionMapping);
 
         // when
-        List<MemberRegionMapping> actual = mappingRegionService.getMemberRegionMappingList(givenMember);
+        List<MemberRegionMapping> actual = memberRegionRepository.findMemberRegionMappingByMember(givenMember);
 
         // then
-        assertThat(actual, is(List.of(givenMemberRegionMapping)));
-    }
-
-    @Test
-    @DisplayName("성공: <MemberRegionMapping> 리스트가 존재한다면 모두 삭제한다.")
-    void deleteMemberRegionTest(){
-        // given
-        Member givenMember = Member.builder()
-                .socialType("kakao")
-                .socialId("99999")
-                .defaultRegion(1L)
-                .nickname("홍길동")
-                .emoji(":)")
-                .build();
-        entityManager.persist(givenMember);
-        Region givenRegion = entityManager.find(Region.class, 1L);
-
-        MemberRegionMapping givenMemberRegionMapping = MemberRegionMapping.builder()
-                .member(givenMember)
-                .region(givenRegion)
-                .regionStatus(RegionEnum.DEFAULT)
-                .build();
-        entityManager.persist(givenMemberRegionMapping);
-
-        // when
-        mappingRegionService.deleteMemberRegion(givenMember);
-
-        // then
-        assertThat(memberRegionRepository.findMemberRegionMappingByMember(givenMember), is(List.of()));
-    }
-
-    @Test
-    @DisplayName("성공: <MemberRegionMapping> 빌드하여 저장한다.")
-    void buildMemberRegionMappingAndSave(){
-        // given
-        Member givenMember = Member.builder()
-                .socialType("kakao")
-                .socialId("99999")
-                .defaultRegion(1L)
-                .nickname("홍길동")
-                .emoji(":)")
-                .build();
-        entityManager.persist(givenMember);
-        Region givenRegion = entityManager.find(Region.class, 1L);
-
-        // when
-        MemberRegionMapping actual = mappingRegionService.buildMemberRegionMappingAndSave(givenMember, givenRegion);
-
-        // then
-        assertThat(actual.getMember(), is(givenMember));
-        assertThat(actual.getRegion(), is(givenRegion));
-        assertThat(actual.getRegionStatus(), is(RegionEnum.DEFAULT));
-    }
-
-
-    @Test
-    @DisplayName("성공: <MemberRegionMapping>을 저장한다.")
-    void saveMemberRegionMapping(){
-        // given
-        Member givenMember = Member.builder()
-                .socialType("kakao")
-                .socialId("99999")
-                .defaultRegion(1L)
-                .nickname("홍길동")
-                .emoji(":)")
-                .build();
-        entityManager.persist(givenMember);
-        Region givenRegion = entityManager.find(Region.class, 1L);
-
-        MemberRegionMapping givenMemberRegionMapping = MemberRegionMapping.builder()
-                .member(givenMember)
-                .region(givenRegion)
-                .regionStatus(RegionEnum.DEFAULT)
-                .build();
-
-        // when
-        MemberRegionMapping actual = mappingRegionService.saveMemberRegionmapping(givenMemberRegionMapping);
-
-        // then
-        assertThat(actual, is(givenMemberRegionMapping));
+        assertThat(actual.get(0).getMember(), is(givenMember));
+        assertThat(actual.get(0).getRegion(), is(givenRegion));
+        assertThat(actual.get(0).getRegionStatus(), is(RegionEnum.DEFAULT));
     }
 }
