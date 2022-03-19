c3153a349f11b406876291974d85e601bd899690
tmddusgood <tmddusgood@gmail.com>
2022-03-19 15:39:23 +0900

    test: 추가 ControllerExceptionHandlerTest

---
+++ misoweather-service/src/test/java/com/misoweather/misoweatherservice/exception/ControllerExceptionHandlerTest.java
@@ -0,0 +1,11 @@
+package com.misoweather.misoweatherservice.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Controller Exception Handler 테스트")
+public class ControllerExceptionHandlerTest {
+
+}
