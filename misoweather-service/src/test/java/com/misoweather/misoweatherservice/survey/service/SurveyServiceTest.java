ae4d5f343e7e0e0f18413fad4116e30cff8ae2f0
tmddusgood <tmddusgood@gmail.com>
2022-03-31 10:18:20 +0900

    test: 추가 - SurveyServiceTest: getAnswerListWhenListNull()

--- misoweather-service/src/test/java/com/misoweather/misoweatherservice/survey/service/SurveyServiceTest.java
+++ misoweather-service/src/test/java/com/misoweather/misoweatherservice/survey/service/SurveyServiceTest.java
@@ -1,8 +1,11 @@
 package com.misoweather.misoweatherservice.survey.service;
 
 import com.misoweather.misoweatherservice.domain.survey.AnswerRepository;
+import com.misoweather.misoweatherservice.domain.survey.Survey;
 import com.misoweather.misoweatherservice.domain.survey.SurveyRepository;
 import com.misoweather.misoweatherservice.global.api.ListDto;
+import com.misoweather.misoweatherservice.global.constants.HttpStatusEnum;
+import com.misoweather.misoweatherservice.global.exception.ApiCustomException;
 import com.misoweather.misoweatherservice.survey.dto.AnswerSurveyJoinDto;
 import com.misoweather.misoweatherservice.survey.dto.AnswerSurveyJoinDtoBuilder;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,11 +18,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static reactor.core.publisher.Mono.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("CommentService 테스트")
@@ -48,5 +55,18 @@ public class SurveyServiceTest {
 
         // then
         assertThat(actual.getResponseList().get(0).getSurveyId(), is(givenSurveyId));
+    }
+
+    @Test
+    @DisplayName("분기 테스트: getAnswerList() survey 존재하지 않으면 에러 발생시킨다.")
+    void getAnswerListWhenListNull(){
+        // given
+        Long givenSurveyId = 99999L;
+        given(answerRepository.findAnswerSurveyJoinBySurveyId(anyLong())).willReturn(List.of());
+
+        // when, then
+        assertThatThrownBy(() -> surveyService.getAnswerList(givenSurveyId))
+                .isInstanceOf(ApiCustomException.class)
+                .hasMessageContaining(HttpStatusEnum.NOT_FOUND.getMessage());
     }
 }
