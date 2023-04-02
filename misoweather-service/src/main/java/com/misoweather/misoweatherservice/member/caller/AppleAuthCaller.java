e549221fc5139b9c42c884404f1f43bf1a6d1935
tmddusgood <tmddusgood@gmail.com>
2023-04-02 10:50:03 +0900

    refactor:

--- misoweather-service/src/main/java/com/misoweather/misoweatherservice/member/caller/AppleAuthCaller.java
+++ misoweather-service/src/main/java/com/misoweather/misoweatherservice/member/caller/AppleAuthCaller.java
@@ -2,27 +2,24 @@ package com.misoweather.misoweatherservice.member.caller;
 
 import com.misoweather.misoweatherservice.global.constants.HttpStatusEnum;
 import com.misoweather.misoweatherservice.global.exception.ApiCustomException;
-import com.misoweather.misoweatherservice.member.builder.AppleAuthCallBuilder;
 import lombok.AllArgsConstructor;
 import org.json.JSONObject;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 
 @AllArgsConstructor
-public class AppleAuthCaller implements ApiCaller {
+@Component
+public class AppleAuthCaller {
+    // Mock 시키기 위해서, 그리고 계속 객체가 생기는 걸 방지하기 위해서 추가한 라인
+    private final AppleAuthCallBuilder appleAuthCallBuilder;
     public JSONObject call() {
-        AppleAuthCallBuilder appleAuthCallBuilder = new AppleAuthCallBuilder();
         appleAuthCallBuilder.addHeader();
         appleAuthCallBuilder.setHttpEntityHeader();
 
         try {
-            ResponseEntity<String> response = appleAuthCallBuilder.restTemplate.exchange(
-                    appleAuthCallBuilder.url,
-                    HttpMethod.GET,
-                    appleAuthCallBuilder.httpEntityHeader,
-                    String.class);
+            ResponseEntity<String> response = appleAuthCallBuilder.exchange();
             return new JSONObject(response.getBody());
         } catch (HttpClientErrorException e) {
             throw new ApiCustomException(HttpStatusEnum
