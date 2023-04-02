7109d7a8ef653060236af774878c1cfa1ba53aff
tmddusgood <tmddusgood@gmail.com>
2023-04-02 18:21:52 +0900

    add: 추가 ValidatorFactory @Component

--- misoweather-service/src/main/java/com/misoweather/misoweatherservice/member/validator/ValidatorFactory.java
+++ misoweather-service/src/main/java/com/misoweather/misoweatherservice/member/validator/ValidatorFactory.java
@@ -3,20 +3,21 @@ package com.misoweather.misoweatherservice.member.validator;
 import com.misoweather.misoweatherservice.global.constants.HttpStatusEnum;
 import com.misoweather.misoweatherservice.global.constants.SocialType;
 import com.misoweather.misoweatherservice.global.exception.ApiCustomException;
-import com.misoweather.misoweatherservice.member.validator.AppleValidator;
-import com.misoweather.misoweatherservice.member.validator.KakaoValidator;
-import com.misoweather.misoweatherservice.member.validator.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ValidatorFactory {
+
     public Validator of(String socialId, String socialType, String socialToken){
         switch (SocialType.getEnum(socialType)){
             case KAKAO:
-                return new KakaoValidator(socialId, socialToken);
+                kakaoValidator.setIdAndToken(socialId, socialToken);
+                return kakaoValidator;
             case APPLE:
-                return new AppleValidator(socialId, socialToken);
+                appleValidator.setIdAndToken(socialId, socialToken);
+                return appleValidator;
             default:
                 throw new ApiCustomException(HttpStatusEnum.NOT_FOUND);
         }
