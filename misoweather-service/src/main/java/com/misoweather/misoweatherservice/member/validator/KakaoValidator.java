11aa04f60c2d3ff0e4264164e4fc63c6bbafe1c0
tmddusgood <tmddusgood@gmail.com>
2023-04-03 17:29:34 +0900

    add: 추가 KakaoValidator Extended, Property Injected, setIdAndToken() Added

--- misoweather-service/src/main/java/com/misoweather/misoweatherservice/member/validator/KakaoValidator.java
+++ misoweather-service/src/main/java/com/misoweather/misoweatherservice/member/validator/KakaoValidator.java
@@ -3,16 +3,25 @@ package com.misoweather.misoweatherservice.member.validator;
 import com.misoweather.misoweatherservice.member.caller.KakaoAuthCaller;
 import lombok.AllArgsConstructor;
 import org.json.JSONObject;
+import org.springframework.stereotype.Component;
 
+@Component
 @AllArgsConstructor
-public class KakaoValidator implements Validator{
+public class KakaoValidator extends Validator{
     String socialId;
     String socialToken;
+    private final KakaoAuthCaller kakaoAuthCaller;
 
+    @Override
     public Boolean valid() {
-        KakaoAuthCaller kakaoAuthCaller = new KakaoAuthCaller(socialToken);
+        kakaoAuthCaller.setSocialToken(socialToken);
         JSONObject jsonObject = kakaoAuthCaller.call();
         if(jsonObject.get("id").toString().equals(socialId)) return Boolean.TRUE;
         return Boolean.FALSE;
+    }
+
+    public void setIdAndToken(String socialId, String socialToken) {
+        this.socialId = socialId;
+        this.socialToken = socialToken;
     }
 }
