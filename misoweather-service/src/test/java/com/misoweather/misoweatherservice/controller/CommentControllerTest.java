497faa9fb8c697e18001c5edd84ae06957ea5c3c
tmddusgood <tmddusgood@gmail.com>
2023-04-13 19:59:07 +0900

    add: 추가 AsciiDoc 일부 도입

--- misoweather-service/src/test/java/com/misoweather/misoweatherservice/controller/CommentControllerTest.java
+++ misoweather-service/src/test/java/com/misoweather/misoweatherservice/controller/CommentControllerTest.java
@@ -12,10 +12,8 @@ import com.misoweather.misoweatherservice.domain.member.Member;
 import com.misoweather.misoweatherservice.global.exception.ControllerExceptionHandler;
 import com.misoweather.misoweatherservice.member.auth.JwtTokenProvider;
 import com.misoweather.misoweatherservice.member.auth.UserDetailsImpl;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,30 +21,41 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
+import static com.misoweather.misoweatherservice.global.formatter.DocumentFormatGenerator.getDateFormat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.spy;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith({RestDocumentationExtension.class})
 @WebMvcTest(controllers = CommentController.class,
         excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
-@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
+@AutoConfigureRestDocs
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("CommentController 테스트")
 public class CommentControllerTest {
     private MockMvc mockMvc;
     @Autowired
+
     private CommentController commentController;
     @Autowired
     private ObjectMapper objectMapper;
@@ -57,17 +66,17 @@ public class CommentControllerTest {
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
 
-    @BeforeAll
-    public void setUp() {
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders
                 .standaloneSetup(commentController)
                 .setControllerAdvice(new ControllerExceptionHandler())
-                .build();
+                .apply(documentationConfiguration(restDocumentation)).build();
     }
 
     @Test
     @DisplayName("성공: registerComment() 코멘트 등록한다")
-    public void registerComment() throws Exception{
+    public void registerComment() throws Exception {
         // given
         Member givenMember = spy(Member.class);
         CommentRegisterRequestDto givenCommentRegisterRequestDto = CommentRegisterRequestDtoBuilder.build("안녕하세요");
@@ -87,12 +96,30 @@ public class CommentControllerTest {
         result
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.commentList[0].content").value(equalTo("안녕하세요")))
-                .andDo(print());
+                .andDo(
+                        document("comment-post",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestFields(
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("comment 내용")
+                                ),
+                                responseFields(
+                                        fieldWithPath("status").type(JsonFieldType.STRING).description("상태"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메시지"),
+                                        fieldWithPath("data.commentList[].createdAt").type(JsonFieldType.STRING).attributes(getDateFormat()).description("생성시간").optional(),
+                                        fieldWithPath("data.commentList[].id").type(JsonFieldType.STRING).description("아이디").optional(),
+                                        fieldWithPath("data.commentList[].content").type(JsonFieldType.STRING).description("내용").optional(),
+                                        fieldWithPath("data.commentList[].bigScale").type(JsonFieldType.STRING).description("지역 큰 단위").optional(),
+                                        fieldWithPath("data.commentList[].nickname").type(JsonFieldType.NUMBER).description("닉네임").optional(),
+                                        fieldWithPath("data.commentList[].deleted").type(JsonFieldType.BOOLEAN).description("삭제 여부").optional(),
+                                        fieldWithPath("data.commentList[].emoji").type(JsonFieldType.STRING).description("이모지").optional()
+                                )
+                        ));
     }
 
     @Test
     @DisplayName("성공: getCommentList() 코멘트 조회 - commentId 있는 경우")
-    public void getCommentList() throws Exception{
+    public void getCommentList() throws Exception {
         // given
         Comment givenComment = Comment.builder().content("안녕하세요").build();
         CommentListResponseDto givenCommentListResponseDto = CommentListResponseDto.builder()
@@ -110,13 +137,33 @@ public class CommentControllerTest {
         result
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.commentList[0].content").value(equalTo("안녕하세요")))
-                .andDo(print());
+                .andDo(
+                document("comment-get",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("commentId").description("comment 아이디"),
+                                parameterWithName("size").description("comment size")
+                        ),
+                        responseFields(
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메시지"),
+                                fieldWithPath("data.hasNext").type(JsonFieldType.BOOLEAN).description("hasNext").optional(),
+                                fieldWithPath("data.commentList[].createdAt").type(JsonFieldType.STRING).attributes(getDateFormat()).description("생성시간").optional(),
+                                fieldWithPath("data.commentList[].id").type(JsonFieldType.STRING).description("아이디").optional(),
+                                fieldWithPath("data.commentList[].content").type(JsonFieldType.STRING).description("내용").optional(),
+                                fieldWithPath("data.commentList[].bigScale").type(JsonFieldType.STRING).description("지역 큰 단위").optional(),
+                                fieldWithPath("data.commentList[].nickname").type(JsonFieldType.NUMBER).description("닉네임").optional(),
+                                fieldWithPath("data.commentList[].deleted").type(JsonFieldType.BOOLEAN).description("삭제 여부").optional(),
+                                fieldWithPath("data.commentList[].emoji").type(JsonFieldType.STRING).description("이모지").optional()
+                        )
+                ));
     }
 
 
     @Test
     @DisplayName("성공: getCommentList() 코멘트 조회 - commentId 없는 경우")
-    public void getCommentListWhenCommentIdSizeNull() throws Exception{
+    public void getCommentListWhenCommentIdSizeNull() throws Exception {
         // given
         CommentListResponseDto givenCommentListResponseDto = CommentListResponseDto.builder().commentList(List.of()).build();
         given(simpleCommentService.getCommentList(any(), any())).willReturn(givenCommentListResponseDto);
