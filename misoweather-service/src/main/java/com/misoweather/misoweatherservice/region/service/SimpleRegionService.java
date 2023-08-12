67e4a8a7b9e6eb2c11d8fde68b31a5684f05f5dc
tmddusgood <tmddusgood@gmail.com>
2023-08-12 14:19:55 +0900

    add: 추가 SimpleRegionService Kafka SendMessage

--- misoweather-service/src/main/java/com/misoweather/misoweatherservice/region/service/SimpleRegionService.java
+++ misoweather-service/src/main/java/com/misoweather/misoweatherservice/region/service/SimpleRegionService.java
@@ -4,6 +4,7 @@ import com.misoweather.misoweatherservice.domain.member.Member;
 import com.misoweather.misoweatherservice.domain.member_region_mapping.MemberRegionMapping;
 import com.misoweather.misoweatherservice.domain.region.Region;
 import com.misoweather.misoweatherservice.mapping.service.MappingRegionService;
+import com.misoweather.misoweatherservice.kafka.MemberEventProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +17,14 @@ public class SimpleRegionService {
 
     private final RegionService regionService;
     private final MappingRegionService mappingRegionService;
+    private final MemberEventProducer memberEventProducer;
 
     @Transactional
     public MemberRegionMapping updateRegion(Member member, Long regionId){
         Region targetRegion = regionService.getRegion(regionId);
         List<MemberRegionMapping> memberRegionMappingList = mappingRegionService.getMemberRegionMappingList(member);
-        return regionService.updateEach(memberRegionMappingList, targetRegion);
+        MemberRegionMapping result = regionService.updateEach(memberRegionMappingList, targetRegion);
+        memberEventProducer.sendMessage(member, targetRegion);
+        return result;
     }
 }
